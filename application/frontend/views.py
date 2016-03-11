beb42b6e31a54f05fb9c8522d58ea62e0bb8a5ab
Andy Driver <andy@pagezero.net>
2016-03-11 17:21:40 +0000

    Add interface to link identities

--- application/frontend/views.py
+++ application/frontend/views.py
@@ -1,6 +1,10 @@
 from flask import (
     Blueprint,
-    render_template
+    abort,
+    redirect,
+    render_template,
+    request,
+    url_for
 )
 
 from application.models import User
@@ -12,3 +16,27 @@ frontend = Blueprint('frontend', __name__, template_folder='templates')
 def index():
     users = User.objects.all()
     return render_template('index.html', users=users)
+
+
+def get_or_404(model, **kwargs):
+    try:
+        return model.objects.get(**kwargs)
+
+    except model.DoesNotExist:
+        abort(404)
+
+
+@frontend.route('/links/<id>', methods=['GET', 'POST'])
+def links(id):
+    user = get_or_404(User, id=id)
+
+    if request.method == 'POST':
+        other = get_or_404(User, id=request.form.get('user_id'))
+        user.link(other)
+        return redirect(url_for('.index'))
+
+    if request.is_xhr:
+        return json.dumps({'ids': [
+            u.user_id for u in user.linked_ids]})
+
+    return render_template('links.html', user=user)
--- application/models.py
+++ application/models.py
@@ -6,3 +6,8 @@ class User(db.Document):
     created = db.DateTimeField(required=True)
     last_login = db.DateTimeField()
     nino = db.StringField()
+    linked_ids = db.ListField(db.ReferenceField('User'), default=[])
+
+    def link(self, other):
+        self.update(push__linked_ids=other)
+        other.update(push__linked_ids=self)
